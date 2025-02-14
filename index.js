45ed7f6940ccb996208323f377d0585c7fede61b
Miqueas <miqueasgigena18@gmail.com>
2025-02-14 17:12:55 -0300

    Phonebook backend step 2

--- index.js
+++ index.js
@@ -3,101 +3,18 @@ const app = express()
 
 app.use(express.json())
 
-let persons = [
-  { 
-    "id": "1",
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": "2",
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": "3",
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": "4",
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const note = persons.find(note => note.id === id)
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
-})
-
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
+app.get('/api/info', (request, response) => {
+  const time = new Date()
+  response.send(`
+    <p>Phonebook has info for 2 people</p>
+    <p>${time}</p>
+    `)
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id 
-  persons = persons.filter(note => note.id !== id)
-  response.status(204).end()
-})
-
-
-app.post('/api/persons', (request, response) => {
-  const note = request.body
-  console.log(note)
-  response.json(note)
-})
-
-app.post('/api/persons', (request, response) => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => Number(n.id)))
-    : 0 
-  
-  const note = request.body
-  note.id = String(maxId + 1)
-
-  persons = persons.concat(note)
-
-  response.json(note)
-})
-
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
-
-app.post('/api/persons', (request, response) => {
-  const body = request.body
-  
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
-
-  const note = {
-    content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId(),
-  }
-
-  persons = persons.concat(note)
-
-  response.json(note)
-})
-
-
 
 
