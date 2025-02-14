6835a0764d555e6e9918be3d09c3651b0d4dd413
Miqueas <miqueasgigena18@gmail.com>
2025-02-14 17:56:26 -0300

    Phonebook backend step 3

--- index.js
+++ index.js
@@ -3,13 +3,51 @@ const app = express()
 
 app.use(express.json())
 
+let persons = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  },
+  {
+    "id": "5",
+    "name": "Arturo Avellaneda",
+    "number": "39-08-2839281"
+  },
+  {
+    "id": "6",
+    "name": "Joaquin Del Barrio",
+    "number": "39-02-9292838"
+  }
+]
 
-app.get('/api/info', (request, response) => {
-  const time = new Date()
-  response.send(`
-    <p>Phonebook has info for 2 people</p>
-    <p>${time}</p>
-    `)
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id 
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else{
+    response.status(404).json({error: "Not found"}).end()
+  }
 })
 
 const PORT = 3001
