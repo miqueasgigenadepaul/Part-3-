184cea23ec86691a70065055e05b9b3b1f746b55
Miqueas <miqueasgigena18@gmail.com>
2025-02-14 17:47:45 -0300

    Phonebook backend step 3

--- index.js
+++ index.js
@@ -3,13 +3,53 @@ const app = express()
 
 app.use(express.json())
 
+let persons = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  },
+  {
+    "id": "5",
+    "name": "Maria Roosevelt",
+    "number": "39-22-8592013"
+  },
+  {
+    "id": "6",
+    "name": "Joaquin Avellaneda",
+    "number": "12-43-1214553"
+  }
+]
 
-app.get('/api/info', (request, response) => {
-  const time = new Date()
-  response.send(`
-    <p>Phonebook has info for 2 people</p>
-    <p>${time}</p>
-    `)
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id 
+  const person = persons.find(person => person.id === id)
+  if(person){
+    response.json(person)
+  } else {
+    response.status(404).json({error: "Not found"}).end()
+    // if i want to show with a message i can do it like this
+    // response.status(404).jsno({error: "Not found"})
+  }
 })
 
 const PORT = 3001
