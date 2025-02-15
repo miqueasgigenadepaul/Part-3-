4afbb6fb44658b6c98d1b0ec8176f09581458c7a
Miqueas <miqueasgigena18@gmail.com>
2025-02-15 16:17:40 -0300

    uploading rest files and index.js

--- index.js
+++ index.js
@@ -57,6 +57,28 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+const generateId = () => {
+  return Math.floor(Math.random() * 300000000)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body 
+
+  if (!body.name || !body.number){
+    response.status(404).json({
+      error: 'name or number missing'
+    })
+  }
+
+  const newPerson = {
+    id: String(generateId()),
+    name: body.name,
+    number: body.number,
+  }
+  persons = persons.concat(newPerson)
+  response.json(newPerson)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
