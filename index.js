7044864f366da34663c1efc885d487f09492ce13
Miqueas <miqueasgigena18@gmail.com>
2025-02-16 17:17:55 -0300

    Phonebook backend step 7 corrected, because I did the exercise in the code of the lesson

--- index.js
+++ index.js
@@ -3,12 +3,7 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json());
-
-morgan.token('body', (req) => 
-  req.method === 'POST' ? JSON.stringify(req.body) : ''
-);
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(morgan('tiny'))
 
 let persons = [
   { 
