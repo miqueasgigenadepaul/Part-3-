8428b43b4c9aaf7f3f3f9c0f36c16a031956569e
Miqueas <miqueasgigena18@gmail.com>
2025-03-03 20:50:47 -0300

    git feat: lesson Connecting the backend to a database, ...

--- index.js
+++ index.js
@@ -1,100 +1,78 @@
+require('dotenv').config() // it's important that dotenv gets imported before the note model
 const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const app = express() 
+const app = express()
 
-app.use(cors())
-app.use(express.json());
+const Note = require('./models/note')
 
-morgan.token('body', (req) => 
-  req.method === 'POST' ? JSON.stringify(req.body) : ''
-);
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-
-let persons = [
-  { 
-    "id": "1",
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": "2",
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": "3",
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": "4",
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  },
-  {
-    "id": "5",
-    "name": "Arturo Avellaneda",
-    "number": "39-08-2839281"
-  },
-  {
-    "id": "6",
-    "name": "Joaquin Del Barrio",
-    "number": "39-02-9292838"
-  }
+let notes = [
 ]
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
+app.use(express.static('dist'))
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const cors = require('cors')
+
+app.use(cors())
+
+app.use(express.json())
+app.use(requestLogger)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id 
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
-  } else{
-    response.status(404).json({error: "Not found"}).end()
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+// USING DATABASE IN ROUTE HANDLERS
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ error: 'content missing' })
   }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id 
-  persons = persons.filter(person => person.id !== id)
+// fetching an individual note using Mongoose's findById method
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
   response.status(204).end()
 })
 
-const generateId = () => {
-  return Math.floor(Math.random() * 300000000)
-}
+app.use(unknownEndpoint)
 
-app.post('/api/persons', (request, response) => {
-  const body = request.body 
-
-  if (!body.name || !body.number){
-    return response.status(400).json({
-      error: "name or number missing"
-    })
-  }
-  const nameExists = persons.some(person => person.name === body.name)
-  if (nameExists){
-    return response.status(400).json({
-      error: "name must be unique"
-    })
-  }
-  
-  const newPerson = {
-    id: String(generateId()),
-    name: body.name,
-    number: body.number,
-  }
-
-  persons = persons.concat(newPerson)
-  response.json(newPerson)
-})
-
-const PORT = 3001
-  app.listen(PORT, () => {
+const PORT = process.env.PORT
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
