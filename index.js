9a9b5871f2f2c3231b04af254870ca4e66548e60
Miqueas <miqueasgigena18@gmail.com>
2025-03-05 22:50:16 -0300

    git fix: correcting dist

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let persons = [
 
 app.use(express.static('dist'))
 
+
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
