fef3a6ff4ef5104d2beafd04a4026a146fc2380d
Miqueas <miqueasgigena18@gmail.com>
2025-03-07 16:36:18 -0300

    git feat: exercise 3.22, adding eslint

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const express = require('express')
 const app = express()
 const Person = require('./models/person')
 
-let persons = [
-]
-
 app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
@@ -20,9 +17,9 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
   if (error.name === 'CastError'){
-    return response.status(400).send({error: 'malformatted id'})
+    return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({error: error.message})
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -56,16 +53,16 @@ app.post('/api/persons', (request, response, next) => {
     return response.status(400).json({ error: 'content missing' })
   }
 
-  Person.findOne({name : body.name})
+  Person.findOne({ name : body.name })
     .then(existingPerson => {
       if (existingPerson) {
-        // if a person exists, update their phone number with the one provided in the request 
-        existingPerson.phone = body.phone 
+        // if a person exists, update their phone number with the one provided in the request
+        existingPerson.phone = body.phone
         return existingPerson.save()
           .then(updatedPerson => response.json(updatedPerson))
           .catch(error => next(error))
       } else {
-        // if no person exists, create a new one 
+        // if no person exists, create a new one
         const person = new Person({
           name: body.name,
           phone: body.phone,
@@ -77,68 +74,67 @@ app.post('/api/persons', (request, response, next) => {
           })
           .catch(error => next(error))
       }
-    })  
-  })
-  
-  app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
-      .then(person => {
-        if (!person){
-          return response.status(404).json({error: 'Person not found'})
-        }
-        response.json(person)
-      })
-      .catch(error => next(error))
-  })
-  
-  app.get('/info', (request, response) => {
-    Person.countDocuments({})
-      .then(count => {
-        response.send(
-          `<p>Phonebook has info for ${count} people </p>
-            <p>${new Date()}</p>`
-        )
-      })
-      .catch(error => {
-        console.error('Error fetching count:', error)
-        response.status(500).send({error: 'Internal Server Error'})
-      })
-  })
-  
-  app.delete('/api/persons/:id', (request, response) => {
-    Person.findByIdAndDelete(request.params.id)
-      .then(result => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
-  })
-  
-  app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body 
-  
-    // Validate the phone number format
-    const phoneRegex = /^(\d{2}|\d{3})-\d{7,8}$/;
-      if (!phoneRegex.test(body.phone)) {
-      return response.status(400).json({ error: 'Invalid phone number format. Use format XX-XXXXXXX or XXX-XXXXXXX.' })
-    }
+    })
+})
 
-    const person = {
-      name: body.name,
-      phone: body.phone, // use the phone number provided in the request (only add it if it's valid)
-    }
-  
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person){
+        return response.status(404).json({ error: 'Person not found' })
+      }
+      response.json(person)
+    })
+    .catch(error => next(error))
+})
+
+app.get('/info', (request, response) => {
+  Person.countDocuments({})
+    .then(count => {
+      response.send(
+        `<p>Phonebook has info for ${count} people </p>
+            <p>${new Date()}</p>`
+      )
+    })
+    .catch(error => {
+      console.error('Error fetching count:', error)
+      response.status(500).send({ error: 'Internal Server Error' })
+    })
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  // Validate the phone number format
+  const phoneRegex = /^(\d{2}|\d{3})-\d{7,8}$/
+  if (!phoneRegex.test(body.phone)) {
+    return response.status(400).json({ error: 'Invalid phone number format. Use format XX-XXXXXXX or XXX-XXXXXXX.' })
+  }
+
+  const person = {
+    name: body.name,
+    phone: body.phone, // use the phone number provided in the request (only add it if it's valid)
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
     .catch(error => next(error))
-  })
-  
-  app.use(unknownEndpoint)
-  
-  app.use(errorHandler)
-  
-  const PORT = process.env.PORT
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+})
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
--- mongo.js
+++ mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {    
+if (process.argv.length<3) {
   console.log('give password as argument')
   process.exit(1)
 }
@@ -13,8 +13,8 @@ mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    phone: String,
+  name: String,
+  phone: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
@@ -34,9 +34,9 @@ else if (process.argv.length === 5) {
   const name = process.argv[3]
   const phone = process.argv[4]
 
-  const person = new Person({name, phone})
+  const person = new Person({ name, phone })
 
-  person.save().then(result => {
+  person.save().then(() => {
     console.log(`${name}: ${phone}`)
     mongoose.connection.close()
   })
